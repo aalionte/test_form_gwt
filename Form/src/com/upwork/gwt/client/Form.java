9dcd5ed7d88b57b06615cecfedf16e99e034e669
Ana Stoica <ana.stoica@gmx.com>
2017-10-27 01:23:06 +0300

    Add validation for input and change a bit the main class

--- Form/src/com/upwork/gwt/client/Form.java
+++ Form/src/com/upwork/gwt/client/Form.java
@@ -1,14 +1,10 @@
 package com.upwork.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.builder.shared.SelectBuilder;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.DOM;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-import jdk.internal.util.xml.impl.Input;
+import com.upwork.gwt.client.validator.ButtonAction;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
@@ -24,11 +20,12 @@ public class Form implements EntryPoint {
         final FormPanel testForm = new FormPanel("Test Form");
         final TextBox nameInput = new TextBox();
         final ListBox jobSelectBox = new ListBox();
-
+        final CheckBox termsCheckBox = new CheckBox();
 
         //setting the elements of the form
         nameInput.setName("name");
-        nameInput.setText("Enter your name");
+        nameInput.getElement().setPropertyString("id", "name");
+        nameInput.getElement().setPropertyString("placeholder", "Enter your name");
 
         VerticalPanel panel = new VerticalPanel();
         panel.setSpacing(10);
@@ -38,6 +35,9 @@ public class Form implements EntryPoint {
         panel.add(nameInput);
 
         jobSelectBox.setName("job");
+        jobSelectBox.getElement().setPropertyString("id", "job");
+
+        jobSelectBox.addItem("Job", "");
         jobSelectBox.addItem("Tinker", "tinker");
         jobSelectBox.addItem("Taylor", "taylor");
         jobSelectBox.addItem("Sailor", "sailor");
@@ -45,34 +45,22 @@ public class Form implements EntryPoint {
         panel.add(new Label("Job"));
         panel.add(jobSelectBox);
 
-        RootPanel.get("submitForm").add(testForm);
+        termsCheckBox.getElement().setPropertyString("id", "terms");
+        termsCheckBox.setText("I agree to send information\n");
+        panel.add(termsCheckBox);
 
-        panel.add(new Button("Submit", (ClickEvent event) -> {
-            testForm.submit();
-        }));
+        panel.add(submitButton);
+        mainPanel.getElement().setPropertyString("id", "mainPanel");
 
-        // Add an event handler to the form.
-        testForm.addSubmitHandler(event -> {
-            if (nameInput.getText().length() == 0) {
-                Window.alert("The text box must not be empty");
-                event.cancel();
-            }
-        });
-    }
+        testForm.getElement().setPropertyString("id", "testForm");
+//        mainPanel.add(new DivElement().setInnerHTML("Test Form"));
+        mainPanel.add(testForm);
 
-    private static class MyAsyncCallback implements AsyncCallback<String> {
-        private Label label;
+        RootPanel.get().add(mainPanel);
 
-        public MyAsyncCallback(Label label) {
-            this.label = label;
-        }
-
-        public void onSuccess(String result) {
-            label.getElement().setInnerHTML(result);
-        }
-
-        public void onFailure(Throwable throwable) {
-            label.setText("Failed to receive answer from server!");
-        }
+        submitButton.addClickHandler(new ButtonAction());
     }
 }
+
+
+
