df2883c04c1ac3fd32662883a91d546f74607648
Ana Stoica <ana.stoica@gmx.com>
2017-10-29 21:07:06 +0200

    Form without validation (yet)

--- Form/src/com/upwork/gwt/client/Form.java
+++ Form/src/com/upwork/gwt/client/Form.java
@@ -1,10 +1,9 @@
 package com.upwork.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.dom.client.DivElement;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.*;
-import com.upwork.gwt.client.validator.ButtonAction;
-
+import com.upwork.gwt.model.Employee;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>
@@ -14,51 +13,13 @@ public class Form implements EntryPoint {
     /**
      * This is the entry point method.
      */
+
+    private final gwtServiceAsync gwtService = GWT.create(gwtService.class);
+
     public void onModuleLoad() {
-        final VerticalPanel mainPanel = new VerticalPanel();
-        final Button submitButton = new Button("Send");
-        final FormPanel testForm = new FormPanel("Test Form");
-        final TextBox nameInput = new TextBox();
-        final ListBox jobSelectBox = new ListBox();
-        final CheckBox termsCheckBox = new CheckBox();
-
-        //setting the elements of the form
-        nameInput.setName("name");
-        nameInput.getElement().setPropertyString("id", "name");
-        nameInput.getElement().setPropertyString("placeholder", "Enter your name");
-
-        VerticalPanel panel = new VerticalPanel();
-        panel.setSpacing(10);
-        testForm.setWidget(panel);
-
-        panel.add(new Label("Name"));
-        panel.add(nameInput);
-
-        jobSelectBox.setName("job");
-        jobSelectBox.getElement().setPropertyString("id", "job");
-
-        jobSelectBox.addItem("Job", "");
-        jobSelectBox.addItem("Tinker", "tinker");
-        jobSelectBox.addItem("Taylor", "taylor");
-        jobSelectBox.addItem("Sailor", "sailor");
-
-        panel.add(new Label("Job"));
-        panel.add(jobSelectBox);
-
-        termsCheckBox.getElement().setPropertyString("id", "terms");
-        termsCheckBox.setText("I agree to send information\n");
-        panel.add(termsCheckBox);
-
-        panel.add(submitButton);
-        mainPanel.getElement().setPropertyString("id", "mainPanel");
-
-        testForm.getElement().setPropertyString("id", "testForm");
-//        mainPanel.add(new DivElement().setInnerHTML("Test Form"));
-        mainPanel.add(testForm);
-
-        RootPanel.get().add(mainPanel);
-
-        submitButton.addClickHandler(new ButtonAction());
+        Employee employee = new Employee();
+        ValidationView validationView = new ValidationView(employee, gwtService);
+        RootPanel.get().add(validationView);
     }
 }
 
